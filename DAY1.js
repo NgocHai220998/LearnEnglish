c8d35d37058ad7f9465085ef160e5a81c5e78333
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2020-12-28 13:53:33 +0700

    Day1

---
+++ DAY1.js
@@ -0,0 +1,154 @@
+const day1 = [{
+  word: "severe",
+  mean: "Nghiêm trọng, quá mức (adj)"
+}, {
+  word: "practical",
+  mean: "Thực tế (adj)"
+}, {
+  word: "hungry",
+  mean: "Đói (adj)"
+}, {
+  word: "foreign",
+  mean: "Ngoại, bên ngoài, nước ngoài (adj)"
+}, {
+  word: "powerful",
+  mean: "Mạnh mẽ (adj)"
+}, {
+  word: "efficient",
+  mean: "Hiệu quả, có hiệu quả (adj)"
+}, {
+  word: "dramatic",
+  mean: "Đột ngột, ấn tượng, đáng kể (adj)"
+}, {
+  word: "dangerous",
+  mean: "Nguy hiểm (adj)"
+}, {
+  word: "capable",
+  mean: "Có khả năng (adj)"
+}, {
+  word: "accurate",
+  mean: "Chính xác, đúng (adj)"
+}, {
+  word: "relevant",
+  mean: "Có liên quan (adj)"
+}, {
+  word: "legal",
+  mean: "Pháp lý (adj)"
+}, {
+  word: "global",
+  mean: "Toàn cầu, toàn cục (adj)"
+}, {
+  word: "environmental",
+  mean: "Môi trường, liên quan đến môi trường (adj)"
+}, {
+  word: "educational",
+  mean: "Giáo dục (adij)"
+}, {
+  word: "best",
+  mean: "Tốt nhất (adj)"
+}, {
+  word: "right",
+  mean: "Đúng (adj)"
+}, {
+  word: "free",
+  mean: "Tự do, miễn phí (adj)"
+}, {
+  word: "few",
+  mean: "Một số, một vài"
+}, {
+  word: "same",
+  mean: "cùng, giống, tương đồng (adj)"
+}, {
+  word: "another",
+  mean: "Khác (adj)"
+}, {
+  word: "great",
+  mean: "Tuyệt vời, tốt (adj)"
+}, {
+  word: "own",
+  mean: "Riêng (adj)"
+}, {
+  word: "much",
+  mean: "Nhiều (adj)"
+}, {
+  word: "each",
+  mean: "Mỗi"
+}, {
+  word: "any",
+  mean: "Bất kỳ"
+}, {
+  word: "good",
+  mean: "Tốt"
+}, {
+  word: "just",
+  mean: "Chỉ, vừa mới, ngay bây giờ"
+}, {
+  word: "vast",
+  mean: "Lớn, nhiều (adj)"
+}, {
+  word: "total",
+  mean: "Tổng số"
+}, {
+  word: "opposite",
+  mean: "Ngược lại, trái ngược (adj)"
+}, {
+  word: "formal",
+  mean: "Chính thức (adj)"
+}, {
+  word: "fine",
+  mean: "Tốt, ổn (adj)"
+}, {
+  word: "fair",
+  mean: "Công bằng (adj)"
+}, {
+  word: "exact",
+  mean: "Chính xác (adj)"
+}, {
+  word: "extreme",
+  mean: "Cực"
+}, {
+  word: "cool",
+  mean: "Mát mẻ (adj)"
+}, {
+  word: "chemical",
+  mean: "Hóa học (n)"
+}, {
+  word: "thin",
+  mean: "Mỏng, thon (adj)"
+}, {
+  word: "objective",
+  mean: "Mục tiêu (n)"
+}, {
+  word: "deep",
+  mean: "sâu (adj)"
+}, {
+  word: "brown",
+  mean: "màu nâu (adj/n)"
+}, {
+  word: "tough",
+  mean: "Khó khăn (adj)"
+}, {
+  word: "secret",
+  mean: "Bí mật (adj)"
+}, {
+  word: "normal",
+  mean: "Bình thường (adj)"
+}, {
+  word: "fresh",
+  mean: "Tươi mới (adj)"
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}]
