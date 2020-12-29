3d45beb153729002c465b0e39390fd8d737c887c
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2020-12-29 13:07:37 +0700

    DAY3

---
+++ DAY2.js
@@ -0,0 +1,184 @@
+const day2 = [{
+  word: "pile",
+  mean: "Chất đống"
+}, {
+  word: "store",
+  mean: "Lưu trữ, cửa hàng"
+}, {
+  word: "roll",
+  mean: "Cuốn, cuộn, sự lăn tròn, quấn (n, v)"
+}, {
+  word: "serve",
+  mean: "Phục vụ (v)"
+}, {
+  word: "board",
+  mean: "Bảng, Lên(tàu, xe, máy bay)"
+}, {
+  word: "frame",
+  mean: "Khung (n)"
+}, {
+  word: "relax",
+  mean: "Thư giãn, giải trí, nghỉ ngơi (v)"
+}, {
+  word: "pick up",
+  mean: "Nhặt lên (v)"
+}, {
+  word: "push",
+  mean: "Đẩy (v)"
+}, {
+  word: "display",
+  mean: "Trưng bày, hiển thị (v)"
+}, {
+  word: "extend",
+  mean: "Mở rộng (v)"
+}, {
+  word: "sip",
+  mean: "Nhấm nháp, chép chép miệng (v)"
+}, {
+  word: "wind",
+  mean: "Gió, Uốn cong (n, v)"
+}, {
+  word: "browse",
+  mean: "Xem qua, xem qua một chút (v)"
+}, {
+  word: "lie",
+  mean: "Nằm (v)"
+}, {
+  word: "wrap",
+  mean: "Gói, bọc"
+}, {
+  word: "exercise",
+  mean: "Vận động, tập thể dục"
+}, {
+  word: "border",
+  mean: "Bao quanh, viền quanh (v, n)"
+}, {
+  word: "set up",
+  mean: "Sắp xếp, sắp đặt"
+}, {
+  word: "spread",
+  mean: "Trải ra, căng ra"
+}, {
+  word: "stock",
+  mean: "Tích trữ"
+}, {
+  word: "wipe",
+  mean: "Lau chùi (v)"
+}, {
+  word: "dine",
+  mean: "Ăn uống"
+}, {
+  word: "fasten",
+  mean: "Buộc, trói"
+}, {
+  word: "exchange",
+  mean: "Trao đổi (n)"
+}, {
+  word: "to take notes",
+  mean: "Ghi chú"
+}, {
+  word: "full",
+  mean: "Đầy"
+}, {
+  word: "lift",
+  mean: "Nâng lên"
+}, {
+  word: "run",
+  mean: "Chạy"
+}, {
+  word: "sort",
+  mean: "Phân loại"
+}, {
+  word: "sew",
+  mean: "May, khâu (v)"
+}, {
+  word: "put away",
+  mean: "Cất đi"
+}, {
+  word: "spray",
+  mean: "Phun, xịt (v)"
+}, {
+  word: "enter",
+  mean: "Đi vào"
+}, {
+  word: "move",
+  mean: "Di chuyển"
+}, {
+  word: "climb",
+  mean: "Leo, trèo"
+}, {
+  word: "check",
+  mean: "Kiểm tra"
+}, {
+  word: "row",
+  mean: "Chèo thuyền, hàng"
+}, {
+  word: "exit",
+  mean: "Ra khỏi, thoát ra"
+}, {
+  word: "mount",
+  mean: "Gắn lên, đính lên, trèo lên"
+}, {
+  word: "to be seated",
+  mean: "Ngồi"
+}, {
+  word: "dock",
+  mean: "Bến tàu (n)"
+}, {
+  word: "put up",
+  mean: "Dựng lên"
+}, {
+  word: "fall",
+  mean: "Ngã (v)"
+}, {
+  word: "cast",
+  mean: "Đổ bóng, diễn viên, diễn (v, n)"
+}, {
+  word: "post",
+  mean: "Đăng, dán (v)"
+}, {
+  word: "take off",
+  mean: "Cởi ra, cất cánh (v)"
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}, {
+  word: "",
+  mean: ""
+}]
