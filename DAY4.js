1bff0dd7e817dda67b0adf56b87466af6c609a35
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2020-12-30 17:50:26 +0700

    Add Day4

---
+++ DAY4.js
@@ -0,0 +1,361 @@
+const day4 = [{
+  word: "article",
+  mean: "Bài báo (n)"
+}, {
+  word: "item",
+  mean: "Món đồ, một element"
+}, {
+  word: "possible",
+  mean: "Có thể "
+}, {
+  word: "available",
+  mean: "Có sẵn"
+}, {
+  word: "indicate",
+  mean: "Chỉ ra"
+}, {
+  word: "promote",
+  mean: "Quảng bá"
+}, {
+  word: "request",
+  mean: "Yêu cầu"
+}, {
+  word: "assistance",
+  mean: "Sự hỗ trợ, trợ lý"
+}, {
+  word: "interest",
+  mean: "Quan tâm"
+}, {
+  word: "even",
+  mean: "Thậm chí"
+}, {
+  word: "industry",
+  mean: "Ngành nghề"
+}, {
+  word: "community",
+  mean: "Cộng đồng"
+}, {
+  word: "suggest",
+  mean: "Đề xuất"
+}, {
+  word: "reserve",
+  mean: "Đặt chỗ"
+}, {
+  word: "process",
+  mean: "Xử lý"
+}, {
+  word: "corporation",
+  mean: "Tập đoàn"
+}, {
+  word: "type",
+  mean: "Loại, kiểu"
+}, {
+  word: "manufacture",
+  mean: "Sản xuất"
+}, {
+  word: "survey",
+  mean: "Khảo sát"
+}, {
+  word: "feature",
+  mean: "Đặc tính, tính năng"
+}, {
+  word: "meet",
+  mean: "Đáp ứng, cuộc hẹn"
+}, {
+  word: "review",
+  mean: "xem lại, đánh giá"
+}, {
+  word: "consider",
+  mean: "xem xét"
+}, {
+  word: "exhibit",
+  mean: "Triển lãm. trưng bày"
+}, {
+  word: "offer",
+  mean: "Cung cấp, đễ nghị"
+}, {
+  word: "support",
+  mean: "Hỗ trợ"
+}, {
+  word: "submit",
+  mean: "Nộp"
+}, {
+  word: "operate",
+  mean: "Hoạt động, điều hành"
+}, {
+  word: "opportunity",
+  mean: "Cơ hội"
+}, {
+  word: "benefit",
+  mean: "Lợi ích"
+}, {
+  word: "personal",
+  mean: "Cá nhân"
+}, {
+  word: "publish",
+  mean: "Xuất bản, đưa ra thị trường"
+}, {
+  word: "charge",
+  mean: "Tính phí, trả phí"
+}, {
+  word: "annual",
+  mean: "Hàng năm"
+}, {
+  word: "package",
+  mean: "Đóng gói, gói hàng"
+}, {
+  word: "found",
+  mean: "Thành lập (v)"
+}, {
+  word: "needs",
+  mean: "Nhu cầu"
+}, {
+  word: "material",
+  mean: "Chất liệu (n)"
+}, {
+  word: "state",
+  mean: "Đề cập, trạng thái"
+}, {
+  word: "session",
+  mean: "Phiên, buổi"
+}, {
+  word: "access",
+  mean: "Tiếp cận, truy cập"
+}, {
+  word: "cause",
+  mean: "Gây ra, nguyên nhân (v, n)"
+}, {
+  word: "refund",
+  mean: "Hoàn lại (tiền)"
+}, {
+  word: "facility",
+  mean: "Cơ sở"
+}, {
+  word: "regarding",
+  mean: "về (about)"
+}, {
+  word: "advance",
+  mean: "Sự tiến bộ, nâng cao"
+}, {
+  word: "serve",
+  mean: "Phục vụ"
+}, {
+  word: "improve",
+  mean: "Cải thiện"
+}, {
+  word: "appreciate",
+  mean: "Cảm kích"
+}, {
+  word: "policy",
+  mean: "Chính sách"
+}, {
+  word: "process",
+  mean: "Quy trình"
+}, {
+  word: "proposal",
+  mean: "Sự đề xuất"
+}, {
+  word: "subscribe",
+  mean: "Đăng ký, đặt báo"
+}, {
+  word: "regard",
+  mean: "Xem xét"
+}, {
+  word: "schedule",
+  mean: "Lên lịch"
+}, {
+  word: "study",
+  mean: "Nghiên cứu, học"
+}, {
+  word: "workshop",
+  mean: "Hội thảo"
+}, {
+  word: "conduct",
+  mean: "Tiến hành"
+}, {
+  word: "category",
+  mean: "Thể loại"
+}, {
+  word: "among",
+  mean: "Ở giữa"
+}, {
+  word: "original",
+  mean: "Nguyên bản"
+}, {
+  word: "at least",
+  mean: "ít nhất"
+}, {
+  word: "award",
+  mean: "giải thưởng"
+}, {
+  word: "describle",
+  mean: "Mô tả "
+}, {
+  word: "concern",
+  mean: "Sự lo lắng"
+}, {
+  word: "perform",
+  mean: "Biểu diễn"
+}, {
+  word: "in addition to",
+  mean: "Ngoài ra"
+}, {
+  word: "invest",
+  mean: "Đầu tư"
+}, {
+  word: "previous",
+  mean: "Trước đó "
+}, {
+  word: "variety",
+  mean: "Sự đa dạng"
+}, {
+  word: "renovate",
+  mean: "tân trang "
+}, {
+  word: "limited",
+  mean: "Bị giới hạn"
+}, {
+  word: "register",
+  mean: "Đăng ký "
+}, {
+  word: "opening",
+  mean: "Khai trương "
+}, {
+  word: "depart",
+  mean: "Khởi hành , xuất phát "
+}, {
+  word: "amount",
+  mean: "Một Khoản, số lượng"
+}, {
+  word: "based on",
+  mean: "Dựa vào"
+}, {
+  word: "notify",
+  mean: "Thông báo"
+}, {
+  word: "approve",
+  mean: "Đồng ý "
+}, {
+  word: "approval",
+  mean: "Sự đồng ý "
+}, {
+  word: "career",
+  mean: "Sự nghiệp"
+}, {
+  word: "energy",
+  mean: "Năng lượng"
+}, {
+  word: "renew",
+  mean: "Gia hạn"
+}, {
+  word: "agency",
+  mean: "Công ty "
+}, {
+  word: "candidate",
+  mean: "Ứng cử viên, người tham gia phỏng vấn "
+}, {
+  word: "warranty",
+  mean: "bảo hành "
+}, {
+  word: "popular",
+  mean: "Nổi tiếng, phổ biến, được ưa chuộng"
+}, {
+  word: "throughout",
+  mean: "Xuyên suốt "
+}, {
+  word: "lecture",
+  mean: "diễn thuyết "
+}, {
+  word: "region",
+  mean: "vùng, khu vực"
+}, {
+  word: "inquire",
+  mean: "Hỏi"
+}, {
+  word: "analyze",
+  mean: "Phân tích"
+}, {
+  word: "permission",
+  mean: "Sự cho phép "
+}, {
+  word: "further",
+  mean: "Thêm"
+}, {
+  word: "field",
+  mean: "Lĩnh vực"
+}, {
+  word: "guide",
+  mean: "Hướng dẫn "
+}, {
+  word: "knowledge",
+  mean: "Kiến thức "
+}, {
+  word: "upon",
+  mean: "vào lúc"
+}, {
+  word: "apply for",
+  mean: "nộp đơn xin "
+}, {
+  word: "personnel",
+  mean: "Nhân sự"
+}, {
+  word: "release",
+  mean: "Tung ra "
+}, {
+  word: "feature",
+  mean: "Bao gồm "
+}, {
+  word: "delay",
+  mean: "Hoãn, trì hoãn "
+}, {
+  word: "pay",
+  mean: "trả tiền "
+}, {
+  word: "related",
+  mean: "liên quan"
+}, {
+  word: "signup",
+  mean: "Đăng ký (=register)"
+}, {
+  word: "edit",
+  mean: "Chỉnh sửa "
+}, {
+  word: "supervise",
+  mean: "Giám sát"
+}, {
+  word: "advance",
+  mean: "Trước"
+}, {
+  word: "solution",
+  mean: "Giải pháp"
+}, {
+  word: "except",
+  mean: "Ngoại trừ, trừ "
+}, {
+  word: "method",
+  mean: "Phương pháp "
+}, {
+  word: "intern",
+  mean: "Thực tập sinh"
+}, {
+  word: "extend",
+  mean: "Kéo dài, mở rộng"
+}, {
+  word: "response",
+  mean: "Sự phản hồi"
+}, {
+  word: "division",
+  mean: "Bộ phận "
+}, {
+  word: "reach",
+  mean: "Đạt mức, đến được, đến "
+}, {
+  word: "present",
+  mean: "Có mặt, sự có mặt "
+}, {
+  word: "maintain",
+  mean: "Duy trì, bảo trì "
+}, {
+  word: "reach for",
+  mean: "Với, với lấy"
+}]
