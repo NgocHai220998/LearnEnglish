c17ea52c2caa03ab85bdc1f6721a05ddc6db6a02
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-03 22:25:21 +0700

    Add day6

---
+++ DAY6.js
@@ -0,0 +1,322 @@
+const day6 = [{
+  word: "gesture",
+  mean: "Cử chỉ, hành động (n)"
+}, {
+  word: "condition",
+  mean: "Điều kiện (n)"
+}, {
+  word: "sharking hand",
+  mean: "Bắt tay (v)"
+}, {
+  word: "associate",
+  mean: "Hợp tác (v)"
+}, {
+  word: "appeal",
+  mean: "Thu hút"
+}, {
+  word: "candidate",
+  mean: "Ứng cử viên, ứng viên (n)"
+}, {
+  word: "option",
+  mean: "Lựa chọn (n)"
+}, {
+  word: "replace",
+  mean: "Thay thế"
+}, {
+  word: "issue",
+  mean: "Vấn đề, số báo (n)"
+}, {
+  word: "promotion",
+  mean: "Sự thăng chức, sự thúc đẩy (n)"
+}, {
+  word: "shift",
+  mean: "Ca, ca làm việc"
+}, {
+  word: "opening",
+  mean: "Sự khai mạc, khai trương, vị trí còn trống (n)"
+}, {
+  word: "impressed",
+  mean: " Ấn tượng"
+}, {
+  word: "brainstorm",
+  mean: "Động não"
+}, {
+  word: "feedback",
+  mean: " Phản hồi (v)"
+}, {
+  word: "confirm",
+  mean: "Xác nhận (v)"
+}, {
+  word: "domestic",
+  mean: "Nội địa"
+}, {
+  word: "warranty",
+  mean: "Bảo hành"
+}, {
+  word: "cover",
+  mean: "Bao gồm, chi trả, bảo vệ"
+}, {
+  word: "upcoming",
+  mean: "Sắp tới"
+}, {
+  word: "figure out",
+  mean: "Tính toán, tìm ra (v)"
+}, {
+  word: "install",
+  mean: " Lắp đặt, lắp ráp"
+}, {
+  word: "community",
+  mean: "Cộng đồng (n)"
+}, {
+  word: "nominate",
+  mean: "Đề cử (v)"
+}, {
+  word: "publish",
+  mean: " Xuất bản (v)"
+}, {
+  word: "locate",
+  mean: "Xác định vị trí, đặt vào vị trí (v)"
+}, {
+  word: "contractor",
+  mean: " Nhà thầu (n)"
+}, {
+  word: "reject",
+  mean: "Từ chối (v)"
+}, {
+  word: "check out",
+  mean: "Làm thủ tục ra khách sạn, thanh toàn (v)"
+}, {
+  word: "extra",
+  mean: "Thêm (v)"
+}, {
+  word: "relationship",
+  mean: " Mối quan hệ (n)"
+}, {
+  word: "apologize",
+  mean: "Xin lỗi (v)"
+}, {
+  word: "assembly",
+  mean: "Sự lắp ráp (n)"
+}, {
+  word: "reservation",
+  mean: "Đặt chỗ trước (n)"
+}, {
+  word: "assorted",
+  mean: "Đủ loại, hỗn hợp"
+}, {
+  word: "experience",
+  mean: "Kinh nghiệm (n)"
+}, {
+  word: "facility",
+  mean: "Cơ sở vật chất (n)"
+}, {
+  word: "loan",
+  mean: "Khoản vay"
+}, {
+  word: "spot",
+  mean: " Điểm, địa điểm"
+}, {
+  word: "recipe",
+  mean: "Công thức, công thức nấu ăn"
+}, {
+  word: "stop by",
+  mean: "Ghé vào"
+}, {
+  word: "prescription",
+  mean: "Đơn thuốc, toa thuốc"
+}, {
+  word: "put together",
+  mean: "Lắp ghép, tổ chức"
+}, {
+  word: "spacious",
+  mean: "Rộng rãi (adj)"
+}, {
+  word: "potential",
+  mean: "Tiềm năng (adj)"
+}, {
+  word: "improvement",
+  mean: "Sự cải thiện (n)"
+}, {
+  word: "overseas",
+  mean: "Ở nước ngoài"
+}, {
+  word: "lease",
+  mean: "Hợp đồng cho thuê, cho thuê, thuê (n, v)"
+}, {
+  word: "remodeling",
+  mean: "Việc tu sửa lại (n)"
+}, {
+  word: "raise",
+  mean: "Tăng, nâng (v)"
+}, {
+  word: "in the meantime",
+  mean: "Trong khi chờ đợi"
+}, {
+  word: "account",
+  mean: "Tài khoản"
+}, {
+  word: "expand",
+  mean: "Mở rộng, thổi phồng lên (v)"
+}, {
+  word: "annual",
+  mean: "Hàng năm"
+}, {
+  word: "promote",
+  mean: "Khuyến khích, thúc đẩy, quảng bá"
+}, {
+  word: "deal",
+  mean: "Thỏa thuận"
+}, {
+  word: "space",
+  mean: "Không gian"
+}, {
+  word: "popular",
+  mean: "Phổ biến, nổi tiếng"
+}, {
+  word: "postpone",
+  mean: "Trì hoãn, hoãn lại"
+}, {
+  word: "budget",
+  mean: "Ngân sách"
+}, {
+  word: "cut back",
+  mean: "Cắt giảm"
+}, {
+  word: "qualified",
+  mean: "Đủ khả năng, đủ điều kiện, đủ năng lực, đủ tiêu chuẩn (adj)"
+}, {
+  word: "neighborhood",
+  mean: "Khu vực lân cận"
+}, {
+  word: "away",
+  mean: "Xa (adj)"
+}, {
+  word: "correct",
+  mean: "Chính xác, không sai (adj)"
+}, {
+  word: "specialize",
+  mean: "Chuyên về, chuyên môn hóa (v)"
+}, {
+  word: "inconvenience",
+  mean: "Không thuận lợi, bất tiện (adj)"
+}, {
+  word: "reasonable",
+  mean: "Hợp lý (adj)"
+}, {
+  word: "turn out",
+  mean: "Biến mất, kết thúc"
+}, {
+  word: "advantage",
+  mean: "Lợi thế"
+}, {
+  word: "own",
+  mean: "Sở hữu, riêng"
+}, {
+  word: "workload",
+  mean: "Khối lượng công việc"
+}, {
+  word: "broken",
+  mean: "Bị hỏng"
+}, {
+  word: "misplace",
+  mean: "Để không đúng chỗ"
+}, {
+  word: "application",
+  mean: "Đơn xin Việc, ứng dụng (n)"
+}, {
+  word: "connect",
+  mean: "Kết nối (v)"
+}, {
+  word: "policy",
+  mean: "Chính sách (n)"
+}, {
+  word: "sound",
+  mean: "Âm thanh, tiếng động"
+}, {
+  word: "handout",
+  mean: "Tài liệu phát tay (n)"
+}, {
+  word: "accommodate",
+  mean: "Chứa, cung cấp (v)"
+}, {
+  word: "rent",
+  mean: "Thuê"
+}, {
+  word: "crowded",
+  mean: " Đông đúc (adj)"
+}, {
+  word: "see if",
+  mean: "Xem ... có hay không"
+}, {
+  word: "release",
+  mean: "Phát hành (v)"
+}, {
+  word: "expense",
+  mean: "Chi phí, kinh phí (n)"
+}, {
+  word: "putting on",
+  mean: "Đăng mặc (v)"
+}, {
+  word: "set aside",
+  mean: "Để dành, để riêng ra"
+}, {
+  word: "background",
+  mean: "lý lịch, chuyên môn"
+}, {
+  word: "author",
+  mean: "Tác giả"
+}, {
+  word: "avaluate",
+  mean: "Đánh giá (v)"
+}, {
+  word: "avaluation",
+  mean: "Sự đánh giá (n)"
+}, {
+  word: "regional",
+  mean: "Khu vực (adj)"
+}, {
+  word: "shape",
+  mean: "Hình dạng"
+}, {
+  word: "block",
+  mean: "Chặn (v)"
+}, {
+  word: "suspend",
+  mean: "Đình chỉ, hoãn lại, treo (v)"
+}, {
+  word: "project",
+  mean: "Dự án"
+}, {
+  word: "public",
+  mean: "Công cộng, công chúng"
+}, {
+  word: "correction",
+  mean: "Sự điều chỉnh, sự chỉnh sửa"
+}, {
+  word: "landscape",
+  mean: "Phong cảnh (n) = landscaping"
+}, {
+  word: "injure",
+  mean: "Làm bị thương "
+}, {
+  word: "hurt",
+  mean: "Bị đau, đau, làm đau (v, adj)"
+}, {
+  word: "fill out",
+  mean: "Điền vào (v)"
+}, {
+  word: "fill in",
+  mean: "Lấp đầy (vị trí trống, thông tin bị thiếu)"
+}, {
+  word: "that way",
+  mean: "theo cách đó"
+}, {
+  word: "damaged",
+  mean: "Hư hỏng"
+}, {
+  word: "in time",
+  mean: "Kịp giờ"
+}, {
+  word: "on time",
+  mean: "Đúng giờ"
+}]
