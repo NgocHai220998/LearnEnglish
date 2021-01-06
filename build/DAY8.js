4c5c577205484f1bcf2b5b82083a6b3dd3372bdd
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-06 23:54:47 +0700

    Add Day 8

---
+++ build/DAY8.js
@@ -0,0 +1,289 @@
+const day8 = [{
+  word: "market",
+  mean: "Thị trường, chợ (n)"
+}, {
+  word: "income",
+  mean: "Thu nhập (n)"
+}, {
+  word: "income level",
+  mean: "Mức thu nhập (n)"
+}, {
+  word: "segmentation",
+  mean: "Phân khúc, phân đoạn (n)"
+}, {
+  word: "market segmentation",
+  mean: "Phân khúc, phân đoạn thị trường"
+}, {
+  word: "advertising",
+  mean: "Quảng cảo (n)"
+}, {
+  word: "advertise",
+  mean: " Quảng cáo (v)"
+}, {
+  word: "advertising budget",
+  mean: "Ngân sách quảng cáo (n)"
+}, {
+  word: "pure",
+  mean: "Lành mạnh, tinh khiết, thuần chủng (adj)"
+}, {
+  word: "commercial",
+  mean: "Kinh doanh, thương mại (adj)"
+}, {
+  word: "shelf",
+  mean: "Kệ, giá để đồ đính trên tường (n)"
+}, {
+  word: "shelf life",
+  mean: "Thời hạn sử dụng"
+}, {
+  word: "the top spot",
+  mean: "Đứng đầu bảng "
+}, {
+  word: "consumer survey",
+  mean: "Khảo sát người tiêu dùng"
+}, {
+  word: "slogan",
+  mean: "Khẩu hiệu (n)"
+}, {
+  word: "sales target",
+  mean: "Mục tiêu doanh thu, mục tiêu doanh số (n)"
+}, {
+  word: "leaflet",
+  mean: "Tờ rơi (n)"
+}, {
+  word: "sales",
+  mean: "Doanh số, doanh thu (n)"
+}, {
+  word: "sales leafle",
+  mean: "Tờ rơi quảng cáo, bán hàng"
+}, {
+  word: "marketing",
+  mean: "Tiếp thị (convince mọi người mua sản phẩm)"
+}, {
+  word: "campaign",
+  mean: "Chiến dịch"
+}, {
+  word: "reason",
+  mean: "Lý do, nguyên cớ (n)"
+}, {
+  word: "trademark",
+  mean: "Nhãn hiệu (n)"
+}, {
+  word: "relation",
+  mean: "Quan hệ (n)"
+}, {
+  word: "distribution",
+  mean: "Sự phân bố, sự phân bổ (n)"
+}, {
+  word: "analysis",
+  mean: "Sự phân tích (n)"
+}, {
+  word: "break into",
+  mean: "Đột nhập"
+}, {
+  word: "catalogue",
+  mean: "Danh mục (n)"
+}, {
+  word: "quarter",
+  mean: "Phần tư"
+}, {
+  word: "the first quarter",
+  mean: "Quý 1"
+}, {
+  word: "target market",
+  mean: "Thị trường mục tiêu (n)"
+}, {
+  word: "strong",
+  mean: "mạnh mẽ, chắc chắn (adj)"
+}, {
+  word: "stable",
+  mean: "Chắc chắn, ổn định (adj)"
+}, {
+  word: "true",
+  mean: "Đúng (adj)"
+}, {
+  word: "correct",
+  mean: "Đúng, chính xác (adj)"
+}, {
+  word: "unhappy",
+  mean: "Không vui, buồn, chán (adj)"
+}, {
+  word: "depressed",
+  mean: "Chán nản, suy sụp (adj)"
+}, {
+  word: "weak",
+  mean: "Yếu (adj)"
+}, {
+  word: "infirm",
+  mean: "Yếu, già yếu (adj)"
+}, {
+  word: "especially",
+  mean: "Đặc biệt (adv)"
+}, {
+  word: "shy",
+  mean: "Ngại ngùng, e thẹn, rụt rè"
+}, {
+  word: "nervous",
+  mean: "Lo lắng (adj)"
+}, {
+  word: "accident",
+  mean: "Tai nạn (n)"
+}, {
+  word: "bashful",
+  mean: "Ngượng ngùng, ngại ngùng (adj)"
+}, {
+  word: "uncomfortable",
+  mean: "Không thoải mái, không dễ chịu"
+}, {
+  word: "comfortable",
+  mean: "Thoải mái, dễ chịu (adj)"
+}, {
+  word: "difficult",
+  mean: "Khó, khó khăn (adj)"
+}, {
+  word: "effort",
+  mean: "Sự nỗ lực, sự cố gắng (n)"
+}, {
+  word: "mean",
+  mean: "Nghĩa là, có suy nghĩ như nào về .... (v)"
+}, {
+  word: "fact",
+  mean: "Sự thật, sự thực tế (n)"
+}, {
+  word: "represent",
+  mean: "Đại diện (v)"
+}, {
+  word: "present",
+  mean: "Món quà, quà tặng"
+}, {
+  word: "old",
+  mean: "Cũ, già (adj)"
+}, {
+  word: "exist",
+  mean: "Tồn tại, hiện hữu (v)"
+}, {
+  word: "shoelace",
+  mean: "Dây giày (n)"
+}, {
+  word: "tie",
+  mean: "Buộc, thắt, cột (v)"
+}, {
+  word: "ancient",
+  mean: "Cổ đại, xưa cũ (adj)"
+}, {
+  word: "neighbour",
+  mean: " Hàng xóm (n)"
+}, {
+  word: "outgoing",
+  mean: "cởi mở, thân thiện (adj)"
+}, {
+  word: "rep",
+  mean: "Người đại diện (đại diện để bán hàng)"
+}, {
+  word: "seller",
+  mean: "Người bán (n)"
+}, {
+  word: "sociable",
+  mean: "Thoải mái, hòa đồng (adj)"
+}, {
+  word: "spend",
+  mean: "Dùng, dành, chi tiêu (v)"
+}, {
+  word: "manage",
+  mean: "Quản lý (v)"
+}, {
+  word: "somehow",
+  mean: "Bằng cách nào đó"
+}, {
+  word: "fortune",
+  mean: "Nhiều tiền, rất nhiều tiền, nhiều một thứ nào đó (tiền bạc, của cải)"
+}, {
+  word: "give",
+  mean: "cho, đưa cho (v)"
+}, {
+  word: "confidence",
+  mean: "Sự tự tin, sự tin tưởng"
+}, {
+  word: "positive",
+  mean: "Tích cực (adj)"
+}, {
+  word: "attitude",
+  mean: "Thái độ (n)"
+}, {
+  word: "pleased",
+  mean: "Vừa lòng, hài lòng (adj)"
+}, {
+  word: "optimistic",
+  mean: "Lạc quan, yêu đời (adj)"
+}, {
+  word: "chance",
+  mean: "Cơ hội (n)"
+}, {
+  word: "explain",
+  mean: "Giải thích (v)"
+}, {
+  word: "describe",
+  mean: "Mô tả, diễn tả (v)"
+}, {
+  word: "degree",
+  mean: "Bằng cấp (n)"
+}, {
+  word: "rich",
+  mean: "Giàu có (adj)"
+}, {
+  word: "poor",
+  mean: "Nghèo (adj)"
+}, {
+  word: "immigrant",
+  mean: "Di trú, nhập cư (v)"
+}, {
+  word: "disparate",
+  mean: "Khác biệt (adj)"
+}, {
+  word: "discrepancy",
+  mean: "Sự khác nhau giữa 2 vật cùng tính chất"
+}, {
+  word: "difference",
+  mean: "Sự khác nhau"
+}, {
+  word: "disperse",
+  mean: "Phân tán, giải tán (v)"
+}, {
+  word: "across",
+  mean: "Qua, ngang qua"
+}, {
+  word: "bridge",
+  mean: "Cầu, cây cầu"
+}, {
+  word: "well off",
+  mean: "Khá giả, giàu có (adj)"
+}, {
+  word: "honest",
+  mean: "Trung thực, đáng tin cậy (adj)"
+}, {
+  word: "steal",
+  mean: "trộm cắp, ăn cắp (v)"
+}, {
+  word: "apology",
+  mean: "Lời xin lỗi (n)"
+}, {
+  word: "young",
+  mean: "Trẻ, khỏe (adj)"
+}, {
+  word: "seem",
+  mean: "Hình như, có lẽ, có vẻ, có vẻ như (v)"
+}, {
+  word: "apologize",
+  mean: "xin lỗi (v)"
+}, {
+  word: "sincere",
+  mean: "Chân thành (adj)"
+}, {
+  word: "behaviour",
+  mean: "Hành vi, cử chỉ, cách đối đãi (n)"
+}, {
+  word: "lack",
+  mean: "Thiếu, thiếu hụt (v)"
+}, {
+  word: "appall",
+  mean: "Sợ hãi, kinh hãi (adj)"
+}]
