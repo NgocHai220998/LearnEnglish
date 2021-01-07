f35e31195242f510ee8a543f4c54426d8fc83127
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-07 12:56:58 +0700

    Add day 9

---
+++ build/DAY9.js
@@ -0,0 +1,175 @@
+const day9 = [{
+  word: "important",
+  mean: "Quan trọng (adj)"
+}, {
+  word: "burn",
+  mean: "Cháy, thiêu, đốt, bỏng (v)"
+}, {
+  word: "badly",
+  mean: "Tệ, tồi tệ, nặng nề, sâu sắc (thường nói về những điều tiêu cực) (adj)"
+}, {
+  word: "affected",
+  mean: "Bị ảnh hưởng (adj)"
+}, {
+  word: "effect",
+  mean: "Hiệu ứng, kết quả của một sự thay đổi (n)"
+}, {
+  word: "affect",
+  mean: "Có ảnh hưởng (v)"
+}, {
+  word: "particular",
+  mean: "Cụ thể, đặc biệt (adj)"
+}, {
+  word: "required",
+  mean: "Thiết yếu, cần thiết (adj)"
+}, {
+  word: "accord",
+  mean: "Một hiệp định, hoặc một deal formal"
+}, {
+  word: "necessary",
+  mean: "Cần thiết (adj)"
+}, {
+  word: "in order to",
+  mean: "Để mà, để làm điều gì đó"
+}, {
+  word: "achieve",
+  mean: "Đạt được (v)"
+}, {
+  word: "purpose",
+  mean: "Mục đích (n)"
+}, {
+  word: "bright",
+  mean: "Sáng, tươi sáng (adj)"
+}, {
+  word: "artist",
+  mean: "Họa sĩ, nghệ sĩ (n)"
+}, {
+  word: "smart",
+  mean: "Thông minh, sạch sẽ, sáng sủa"
+}, {
+  word: "appearance",
+  mean: "Sự xuất hiện, vẻ bề ngoài"
+}, {
+  word: "occasion",
+  mean: "một dịp, một đợt (n)"
+}, {
+  word: "special",
+  mean: "Đặc biệt (adj)"
+}, {
+  word: "idle",
+  mean: "Nhàn rỗi, yên (adj)"
+}, {
+  word: "factory",
+  mean: "Nhà máy, xưởng sản xuất (n)"
+}, {
+  word: "lazy",
+  mean: "Lười biếng, lười nhác"
+}, {
+  word: "complain",
+  mean: "Phàn nàn (v)"
+}, {
+  word: "noise",
+  mean: "Tiếng ồn, ồn ào (n)"
+}, {
+  word: "unpleasant",
+  mean: "Khó chịu, không thoải mái (adj)"
+}, {
+  word: "pleasant",
+  mean: "Dễ chịu, thoải mái (adj) comfortable"
+}, {
+  word: "surprise",
+  mean: "Sự bất ngờ (n)"
+}, {
+  word: "lucky",
+  mean: "May mắn (adj)"
+}, {
+  word: "escape",
+  mean: "Trốn thoát, thoát khỏi"
+}, {
+  word: "notice",
+  mean: "Nhận thấy, nhận ra, chú ý (v)"
+}, {
+  word: "fortunate",
+  mean: "May mắn (adj)"
+}, {
+  word: "quality",
+  mean: "Chất lượng (n)"
+}, {
+  word: "terrible",
+  mean: "Kinh khủng, kinh hoàng, khủng khiếp (adj)"
+}, {
+  word: "traffic",
+  mean: "Lưu lượng, giao thông (n)"
+}, {
+  word: "baffle",
+  mean: "Vách ngăn, làm trở ngại (n, v)"
+}, {
+  word: "strange",
+  mean: "Lạ, lạ lùng, kỳ lạ (adj)"
+}, {
+  word: "tourist",
+  mean: "Khách du lịch (n)"
+}, {
+  word: "confuse",
+  mean: "Bối rối, khó hiểu (v)"
+}, {
+  word: "unnecessarily",
+  mean: "Không cần thiết (adv)"
+}, {
+  word: "beautiful",
+  mean: "Đẹp (adj)"
+}, {
+  word: "dress",
+  mean: "Váy, cái váy (n)"
+}, {
+  word: "scenery",
+  mean: "Cảnh, phong cảnh (n)"
+}, {
+  word: "attractive",
+  mean: "Đẹp, hấp dẫn (adj) = fascinating"
+}, {
+  word: "interesting",
+  mean: "Thú vị (adj)"
+}, {
+  word: "situation",
+  mean: "Tình hình, tình huống (n)"
+}, {
+  word: "fair",
+  mean: "Công bằng (adj)"
+}, {
+  word: "allow",
+  mean: "Cho phép (v)"
+}, {
+  word: "prevent",
+  mean: "Ngăn chặn (v)"
+}, {
+  word: "claim",
+  mean: "Tuyên bố (v) một điều gì đó là đúng sự thật"
+}, {
+  word: "funny",
+  mean: "Hài hước, khôi hài (adj)"
+}, {
+  word: "laughter",
+  mean: "Tiếng cười (n)"
+}, {
+  word: "laugh",
+  mean: "Cười (v)"
+}, {
+  word: "humorous",
+  mean: "Hài hước, khôi hài (adj)"
+}, {
+  word: "teenage",
+  mean: "Tuổi teen, giới trẻ"
+}, {
+  word: "happy",
+  mean: "Hạnh phúc, vui vẻ (adj)"
+}, {
+  word: "joyful",
+  mean: "Vui vẻ, hân hoan, hạnh phúc (adj)"
+}, {
+  word: "moment",
+  mean: "Một chút (n)"
+}, {
+  word: "expect",
+  mean: "Mong muốn, mong đợi, kỳ vọng (v)"
+}]
