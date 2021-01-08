5007855485c848afc49aa4e8fe40d16578fe9905
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-08 23:57:50 +0700

    Update Day9

--- build/DAY10.js
+++ build/DAY10.js
@@ -85,4 +85,178 @@ const day10 = [{
 }, {
   word: "emotion",
   mean: "Cảm xúc, xúc động, cảm động (n)"
+}, {
+  word: "completely",
+  mean: "Hoàn toàn (adv)"
+}, {
+  word: "obligate",
+  mean: "Bắt buộc, ép buộc, cướng bức (v)"
+}, {
+  word: "obligation",
+  mean: "Nghĩa vụ, bổn phận (n)"
+}, {
+  word: "obligated",
+  mean: "Nghĩa vụ, tính bắt buộc (adj)"
+}, {
+  word: "ensure",
+  mean: "Đảm bảo (v)"
+}, {
+  word: "safety",
+  mean: "An toàn (n)"
+}, {
+  word: "force",
+  mean: "Bắt buộc, cướng bức, mạnh mẽ"
+}, {
+  word: "instructions",
+  mean: "Sự hướng dẫn (n)"
+}, {
+  word: "certain",
+  mean: "Chắc chắn (adj)"
+}, {
+  word: "government",
+  mean: "Chính phủ (n)"
+}, {
+  word: "national",
+  mean: "Quốc gia, tính quốc gia (adj)"
+}, {
+  word: "political",
+  mean: "Chính trị, tính chính trị (adj)"
+}, {
+  word: "election",
+  mean: "Cuộc bầu cử (n)"
+}, {
+  word: "compete",
+  mean: "Cạnh tranh, ganh đua (v)"
+}, {
+  word: "successful",
+  mean: "Thành công, tính thành công (adj)"
+}, {
+  word: "resolve",
+  mean: "Giải quyết (v)"
+}, {
+  word: "difficultly",
+  mean: "Khó khăn, độ khó, sự khó khăn (n)"
+}, {
+  word: "dispute",
+  mean: "Sự tranh chấp (n)"
+}, {
+  word: "terms",
+  mean: "Điều khoản (n)"
+}, {
+  word: "specify",
+  mean: "Xác định rõ, chỉ rõ, ghi rõ"
+}, {
+  word: "clearly",
+  mean: "Rõ ràng (adj) = obvious"
+}, {
+  word: "exactly",
+  mean: "Đúng, chính xác (adv)"
+}, {
+  word: "reliable",
+  mean: "Đáng tin cậy (adj)"
+}, {
+  word: "advice",
+  mean: "Lời khuyên (n)"
+}, {
+  word: "withdraw",
+  mean: "Rút, thu hồi (v)"
+}, {
+  word: "withdrawal",
+  mean: "Sự rút, sự rút tiền (n)"
+}, {
+  word: "attract",
+  mean: "Thu hút, hấp dẫn (v)"
+}, {
+  word: "widespread",
+  mean: "Rộng rãi, phổ biến, lan rộng (adj)"
+}, {
+  word: "compare",
+  mean: "So sánh, so với (v)"
+}, {
+  word: "expensive",
+  mean: "Đắt đỏ, đắt (adj)"
+}, {
+  word: "sincerely",
+  mean: "chân thành, trân trọng (adv)"
+}, {
+  word: "examine",
+  mean: "Xem xét, kiểm tra (v)"
+}, {
+  word: "discover",
+  mean: "Khám phá, phát hiện ra (v) đặc biệt là lần đâu, có cảm giác bất ngờ"
+}, {
+  word: "persuade",
+  mean: "Thuyết phục (v) = convince"
+}, {
+  word: "innocent",
+  mean: "Vô tội, không biết gì cả (adj)"
+}, {
+  word: "firmly",
+  mean: "Cứng rắn, kiên định, vững chắc (adv)"
+}, {
+  word: "fad",
+  mean: "Mốt (n)"
+}, {
+  word: "activity",
+  mean: "Hoạt động (n)"
+}, {
+  word: "persist",
+  mean: "kéo dài, tiếp diễn (v)"
+}, {
+  word: "implementation",
+  mean: "Sự thực hiện, sự triển khai (n)"
+}, {
+  word: "implement",
+  mean: "Thực hiện, triển khai"
+}, {
+  word: "expend",
+  mean: "Tiêu sài, chi tiêu, tiêu tốn, hao phí (v)"
+}, {
+  word: "preparation",
+  mean: "Sự chuẩn bị (n)"
+}, {
+  word: "prepare",
+  mean: "Chuẩn bị (v)"
+}, {
+  word: "inspire",
+  mean: "Truyền cảm hứng, truyền hứng thú cho ai đó (v)"
+}, {
+  word: "determination",
+  mean: "Sự quết tâm (n)"
+}, {
+  word: "ability",
+  mean: "Khả năng (n)"
+}, {
+  word: "although",
+  mean: "Mặc dù"
+}, {
+  word: "despite",
+  mean: "Bất chấp, mặc dù"
+}, {
+  word: "productive",
+  mean: "Năng suất, hiệu quả (adj)"
+}, {
+  word: "unproductive",
+  mean: "Không năng suất, không hiệu quả (adj)"
+}, {
+  word: "imitation",
+  mean: "Giả mạo, bắt chước (adj)"
+}, {
+  word: "satisfy",
+  mean: "Làm hài lòng, làm thỏa mãn (v)"
+}, {
+  word: "curiosity",
+  mean: "Sự tò mò, sự hiếu kỳ (n)"
+}, {
+  word: "excite",
+  mean: "Kích thích (v)"
+}, {
+  word: "enthusiasm",
+  mean: "Sự nhiệt tình, sự hăng hái (n)"
+}, {
+  word: "arouse",
+  mean: "Khơi dậy, đánh thức (v)"
+}, {
+  word: "enthusiastic",
+  mean: "Nhiện tình, hăng hái (adj)"
 }]
