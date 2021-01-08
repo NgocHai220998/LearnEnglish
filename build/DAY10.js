6b62d7653e98ac36019433df8b145734e23e2472
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-08 21:34:59 +0700

    add DAY10

---
+++ build/DAY10.js
@@ -0,0 +1,88 @@
+const day10 = [{
+  word: "determine",
+  mean: "Quyết định, định trước, xác định (v)"
+}, {
+  word: "decide",
+  mean: "Quyết định (v)"
+}, {
+  word: "several",
+  mean: "Một vài, một số"
+}, {
+  word: "choose",
+  mean: "Chọn (v)"
+}, {
+  word: "carefully",
+  mean: "Cẩn thận (adv)"
+}, {
+  word: "careful",
+  mean: "Cẩn thận (adj)"
+}, {
+  word: "mistake",
+  mean: "Lỗi, sự nhầm lẫn (n)"
+}, {
+  word: "unable",
+  mean: "Không thể (adj)"
+}, {
+  word: "reputation",
+  mean: "Danh tiếng (n)"
+}, {
+  word: "harm",
+  mean: "Làm hạ, làm tổn thương (v)"
+}, {
+  word: "possibility",
+  mean: "Khả năng (n)"
+}, {
+  word: "likely",
+  mean: "Có thể (adj)"
+}, {
+  word: "damage",
+  mean: "Hư hại, tổn hại, (v)"
+}, {
+  word: "probable",
+  mean: "Có thể, có khả năng (adj)"
+}, {
+  word: "spoil",
+  mean: "Làm hỏng (v)"
+}, {
+  word: "reduce",
+  mean: "Giảm (v)"
+}, {
+  word: "become",
+  mean: "Trở nên"
+}, {
+  word: "suspicious",
+  mean: "Nghi ngờ, khả nghi (adj)"
+}, {
+  word: "employ",
+  mean: "Tuyển, tuyển dụng (v)"
+}, {
+  word: "employee",
+  mean: "Nhân viên (n)"
+}, {
+  word: "engage",
+  mean: "Tuyển, tuyển dụng, thuê (v)"
+}, {
+  word: "administration",
+  mean: "Quản trị (n)"
+}, {
+  word: "reputable",
+  mean: "Có danh tiếng, có uy tín, có tiếng (adj)"
+}, {
+  word: "establish",
+  mean: "Thành lập, thiết lập (v)"
+}, {
+  word: "century",
+  mean: "Thế kỷ (n)"
+}, {
+  word: "scarcely",
+  mean: "Hầu như không (adv)"
+}, {
+  word: "communication",
+  mean: "Giao tiếp (n)"
+}, {
+  word: "communicate",
+  mean: "Giao tiếp, truyền đạt (v)"
+}, {
+  word: "emotion",
+  mean: "Cảm xúc, xúc động, cảm động (n)"
+}]
