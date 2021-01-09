0fad70708cd2e49256a28cc9a18b16637a323d71
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-09 18:31:31 +0700

    Add day 11

---
+++ build/DAY11.js
@@ -0,0 +1,190 @@
+const day11 = [{
+  word: "suitcase",
+  mean: "Va li, cái va li (n)"
+}, {
+  word: "conversation",
+  mean: "Cuộc hội thoại, cuộc trò truyện"
+}, {
+  word: "conference",
+  mean: "Hội nghị, cuộc thảo luận (n) = discussion"
+}, {
+  word: "delegate",
+  mean: "đại biểu, ủy thác, giao phó (n)"
+}, {
+  word: "characteristic",
+  mean: "Đặc tính, đặc thù, đặc trưng (adj) (n)"
+}, {
+  word: "characteristically",
+  mean: "Đặc thù, đặc sắc (adv)"
+}, {
+  word: "behave",
+  mean: "cư xử, hành động (v)"
+}, {
+  word: "performance",
+  mean: "Hiệu suất (n)"
+}, {
+  word: "confidential",
+  mean: "Tuyệt mật, bí mật (adj)"
+}, {
+  word: "manner",
+  mean: "cách thức (n)"
+}, {
+  word: "general",
+  mean: "Chung (adj)"
+}, {
+  word: "justice",
+  mean: "Công bằng, công lý (n)"
+}, {
+  word: "impartiality",
+  mean: "Sự không thiên vị (n)"
+}, {
+  word: "suffer",
+  mean: "Sự trải qua nỗi đau về thể xác hoặc tinh thần (n)"
+}, {
+  word: "mediate",
+  mean: "Hòa giải (v)"
+}, {
+  word: "separate",
+  mean: "Độc lập, tách biệt, riêng biệt (adj)"
+}, {
+  word: "department",
+  mean: "Bộ phận, phòng ban, khoa (n)"
+}, {
+  word: "fairness",
+  mean: "Sự công bằng (n)"
+}, {
+  word: "injustice",
+  mean: "Sự không công bằng, sự bất công (n)"
+}, {
+  word: "sense",
+  mean: "Giác quan (n)"
+}, {
+  word: "emotional",
+  mean: "Tình cảm, tính cảm xúc (adj)"
+}, {
+  word: "hear",
+  mean: "Nghe (v)"
+}, {
+  word: "smell",
+  mean: "Mùi, khứu giác (n)"
+}, {
+  word: "taste",
+  mean: "Hương vị, xúc giác (n)"
+}, {
+  word: "independently",
+  mean: "Độc lập, tách biêt (adv)"
+}, {
+  word: "independent",
+  mean: "Độc lập, không phụ thuộc (adj)"
+}, {
+  word: "inquiry = enquiry",
+  mean: "Sự điều tra, hỏi thăm (n)"
+}, {
+  word: "profession",
+  mean: "Nghề nghiệp (n) = industry"
+}, {
+  word: "entitle",
+  mean: "Cho phép, cấp quyền, cho quyền (v)"
+}, {
+  word: "occupation",
+  mean: "Nghề nghiệp (n) = industry = profession"
+}, {
+  word: "regular",
+  mean: "Thường xuyên (adj)"
+}, {
+  word: "regulation",
+  mean: "Quy định, quy tắc (n) rule"
+}, {
+  word: "research",
+  mean: "Nghiên cứu, tự học (v)"
+}, {
+  word: "professional",
+  mean: "Chuyên gia, chuyên nghiệp (adj)"
+}, {
+  word: "inform",
+  mean: "Thông báo (v)"
+}, {
+  word: "addition",
+  mean: "Sự thêm vào (n)"
+}, {
+  word: "involve",
+  mean: "Liên quan (v)"
+}, {
+  word: "involved",
+  mean: "Liên quan, dính lứu, lôi kéo (adj)"
+}, {
+  word: "relative",
+  mean: "Họ hàng (n)"
+}, {
+  word: "respected",
+  mean: "Tôn trọng, kính trọng (adj)"
+}, {
+  word: "respect",
+  mean: "Sự tôn trọng, sự kính trọng (n)"
+}, {
+  word: "concentrate",
+  mean: "Tập trung, chú tâm (v)"
+}, {
+  word: "writing",
+  mean: "Bài viết (n)"
+}, {
+  word: "convey",
+  mean: "Truyền đạt, truyền tải, chuyển lời (v)"
+}, {
+  word: "excellent",
+  mean: "Xuất sắc, tuyệt vời (adj)"
+}, {
+  word: "extremely",
+  mean: "Vô cùng, cực kỳ (adv)"
+}, {
+  word: "borrow",
+  mean: "Mượn, vay (v)"
+}, {
+  word: "lend",
+  mean: "Cho mượn, cho vay (v)"
+}, {
+  word: "borrower",
+  mean: "Người nợ tiền, người vay tiền (n)"
+}, {
+  word: "breathing",
+  mean: "Thở, hơi thở (n)"
+}, {
+  word: "demolish",
+  mean: "Phá hủy, phá bỏ (v)"
+}, {
+  word: "agent",
+  mean: "Đại lý (n)"
+}, {
+  word: "agency",
+  mean: "Một nhóm đại diện cho một nhóm khác, trao đổi hoặc dosomething điều gì đó, đại lý (n)"
+}, {
+  word: "barrister",
+  mean: "Luật sư (n) = lawyer"
+}, {
+  word: "lawyer",
+  mean: "Luật sư (n) barrister"
+}, {
+  word: "repeatedly",
+  mean: "Liên tục (adv)"
+}, {
+  word: "repeated",
+  mean: "Lặp đi, lặp lại (adj)"
+}, {
+  word: "attempt",
+  mean: "Cố gắng (v)"
+}, {
+  word: "through",
+  mean: "Xuyên qua (pre)"
+}, {
+  word: "entire",
+  mean: "Toàn bộ (adj)"
+}, {
+  word: "undertake",
+  mean: "Thực hiện, tiến hành (v)"
+},{
+  word: "experiment",
+  mean: "Cuộc thực hiện, cuộc thí nghiệm (n)"
+},{
+  word: "",
+  mean: ""
+}]
