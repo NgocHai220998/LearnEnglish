c4b996edd23ee15a7e6fed90e63dc2145d9164ed
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-10 21:06:56 +0700

    Update DAY12

--- build/DAY12.js
+++ build/DAY12.js
@@ -85,6 +85,270 @@ const day12 = [{
 }, {
   word: "innocent",
   mean: "Vô tội, không biết gì cả (adj)"
+},{
+  word: "avoid",
+  mean: "Tránh, né tránh"
+}, {
+  word: "gather",
+  mean: "Tập chung, tụ tập (v)"
+}, {
+  word: "collect",
+  mean: "Sưu tầm, sưu tập, thu dọn"
+}, {
+  word: "hobby",
+  mean: "Sở thích (n) = preference"
+}, {
+  word: "discriminate",
+  mean: "Phân biệt đối xử (v)"
+}, {
+  word: "primary",
+  mean: "Chính, chủ yếu, chủ đạo (adj)"
+}, {
+  word: "responsibility",
+  mean: "Sự chịu trách nhiệm, trách nhiệm (n)"
+}, {
+  word: "responsible",
+  mean: "Chịu trách nhiệm, có trách nhiệm (adj)"
+}, {
+  word: "possibility",
+  mean: "Khả năng (n)"
+}, {
+  word: "risk",
+  mean: "Sự rủi ro, nguy cơ, mối nguy hiểm (n)"
+}, {
+  word: "such as",
+  mean: "Như, như là ..."
+}, {
+  word: "strategy",
+  mean: "Chiến lược"
+}, {
+  word: "emergency",
+  mean: "Khẩn cấp, trường hợp khẩn cấp (n)"
+}, {
+  word: "suddenly",
+  mean: "Đột ngột, bất chợt, bất ngờ (adv)"
+}, {
+  word: "sudden",
+  mean: "Đột ngột, đột nhiên, bất chợt (adj)"
+}, {
+  word: "unexpectedly",
+  mean: "Ngoài mong đợi, bất ngờ (adv)"
+}, {
+  word: "substitute",
+  mean: "Thay thế, thế chỗ (v)"
+}, {
+  word: "instead",
+  mean: "Thay cho ... (adv)"
+}, {
+  word: "association",
+  mean: "Tổ chức, hiệp hội (n)"
+}, {
+  word: "get in touch",
+  mean: "Liên lạc, liên hệ (n)"
+}, {
+  word: "location",
+  mean: "Vị trí, địa điểm (n)"
+}, {
+  word: "record",
+  mean: "Ghi lại (v)"
+}, {
+  word: "equipment",
+  mean: "Thiết bị, trang thiết bị (n)"
+}, {
+  word: "electrical",
+  mean: "Điện, thuộc về điện (adj)"
+}, {
+  word: "overcrowded",
+  mean: "Quá đông (adj)"
+}, {
+  word: "take part in",
+  mean: "Tham gia, tham dự (v)"
+}, {
+  word: "allocate",
+  mean: "Phân bổ, chia sẻ, phân phát (v)"
+}, {
+  word: "investigate",
+  mean: "Điều tra (v)"
+}, {
+  word: "senior",
+  mean: "Cao cấp, cấp cao (adj)"
+}, {
+  word: "compatible",
+  mean: "Tương thích (adj)"
+}, {
+  word: "duplicate",
+  mean: "Bản sao, trùng lặp, tạo bản sao, sao chép (adj, v)"
+}, {
+  word: "equipped",
+  mean: "Được trang bị (adj)"
+}, {
+  word: "equip",
+  mean: "Trang bị (v)"
+}, {
+  word: "figure out",
+  mean: "Tìm ra, hiểu ra (v)"
+}, {
+  word: "warn",
+  mean: "Cảnh báo (v) warning (n)"
+}, {
+  word: "future",
+  mean: "Tương lai (n) distant future"
+}, {
+  word: "distant",
+  mean: "Xa, xa xôi (adj)"
+}, {
+  word: "affordable",
+  mean: "Giá cả phải chăng, giá cả hợp lý"
+}, {
+  word: "as need",
+  mean: "Khi cần (adv)"
+}, {
+  word: "be in charge of",
+  mean: "Phụ trách"
+}, {
+  word: "capacity",
+  mean: "Sức chứa (n)"
+}, {
+  word: "durable",
+  mean: "Bền (adj)"
+}, {
+  word: "initiative",
+  mean: "Sáng kiến (n)"
+}, {
+  word: "peace",
+  mean: "Hòa bình, yên bình (n)"
+}, {
+  word: "physical",
+  mean: "Vật lý, thuộc về vật chất (adj)"
+}, {
+  word: "provider",
+  mean: "Nhà cung cấp, người cung cấp (n)"
+}, {
+  word: "recur",
+  mean: "Lặp lại, tái diễn (v)"
+}, {
+  word: "recursion",
+  mean: "Đệ quy, sự lồng vào nhau lặp đi lặp lại (n)"
+}, {
+  word: "practice",
+  mean: "Thực tế, trên thực tế (n)"
+}, {
+  word: "reduction",
+  mean: "Sự giảm bớt, sự thu nhỏ (n)"
+}, {
+  word: "stay on top of",
+  mean: "Cập nhật, nắm bắt thông tin mới nhất (v)"
+}, {
+  word: "stock",
+  mean: "Nguồn cung cấp, nguồn hàng, tích trữ (n, v)"
+}, {
+  word: "appreciate",
+  mean: "Đánh giá, nhận thức rõ (v)"
+}, {
+  word: "immediate",
+  mean: "Ngay lập tức (adj)"
+}, {
+  word: "poison",
+  mean: "Thuốc độc, chất độc (n)"
+}, {
+  word: "be exposed to",
+  mean: "Được tiếp xúc với, tiếp xúc với ..."
+}, {
+  word: "bring in",
+  mean: "Thuê, mướn, dẫn tới (v)"
+}, {
+  word: "casual",
+  mean: "Thông thường (adj)"
+}, {
+  word: "glimpse",
+  mean: "Nhìn thoáng qua, cái nhìn thoáng qua (v, n)"
+}, {
+  word: "beach",
+  mean: "Bãi biển (n)"
+}, {
+  word: "solid",
+  mean: "Rắn, chắc (adj)"
+}, {
+  word: "made of",
+  mean: "Làm từ (v) làm từ chất liệu gì ... vd: cái áo này làm từ nhung"
+}, {
+  word: "out of",
+  mean: "Hết, hết cái gì đó (adj)"
+}, {
+  word: "reinforce",
+  mean: "Củng cố, gia cố, (v) làm cho cái gì đó trở nên vững chắc"
+}, {
+  word: "proof",
+  mean: "Bằng chức, tìm lỗi (n, v)"
+}, {
+  word: "piece",
+  mean: "Cái, một phần, một cái (n)"
+}, {
+  word: "verbal",
+  mean: "Bằng lời nói (adj) (ko viết ra mà nói)"
+}, {
+  word: "facilitate",
+  mean: "Tạo điều kiện, làm cho cái gì đó thuận lợi (v)"
+}, {
+  word: "network",
+  mean: "Mạng lưới (n)"
+}, {
+  word: "popularity",
+  mean: "Sự phổ biến, sự được ưa chuộng"
+}, {
+  word: "revolution",
+  mean: "Cuộc cách mạng (n)"
+}, {
+  word: "sharp",
+  mean: "Nhọn, sắc, khôn ngoan (adj)"
+}, {
+  word: "skill",
+  mean: "Kỹ năng (n)"
+}, {
+  word: "storage",
+  mean: "Nơi lưu trữ, kho (n)"
+}, {
+  word: "beforehand",
+  mean: "Từ trước, trước đó (adv)"
+}, {
+  word: "complicated",
+  mean: "Phức tạp, khó hiểu (adj)"
+}, {
+  word: "mention",
+  mean: "Đề cập đến, nhắc đến, sự đề cập đến, sự nhắc đến (v, n)"
+}, {
+  word: "petition",
+  mean: "Đơn yêu cầu, đơn kiến nghị (n)"
+}, {
+  word: "abundant",
+  mean: "Nhiều, phong phú, thừa thãi (adj)"
+}, {
+  word: "accomplishment",
+  mean: "Thành quả, thành tựu (n)"
+}, {
+  word: "bring together",
+  mean: "Tập hợp lại, thu thập lại (v)"
+}, {
+  word: "come up with",
+  mean: "Nghĩ ra, tạo ra (v)"
+}, {
+  word: "commensurate",
+  mean: "Tưng xứng với, tương ứng với, phù hợp với"
+}, {
+  word: "match",
+  mean: "Sự phù hợp (n)"
+}, {
+  word: "qualification",
+  mean: "Bằng cấp, trình độ, giấy tờ demonstrate trình độ specialize của bạn"
+}, {
+  word: "time-consuming",
+  mean: "Tốn thời gian (adj)"
+}, {
+  word: "ability",
+  mean: "Năng lực, khả năng (n)"
+}, {
+  word: "",
+  mean: ""
 }, {
   word: "",
   mean: ""
