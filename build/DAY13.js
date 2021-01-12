32f793f79fdaf9a90cc6682e9617f9cda5a075c4
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-12 07:48:40 +0700

    Add Day13

---
+++ build/DAY13.js
@@ -0,0 +1,88 @@
+const day13 = [{
+  word: "enterprise",
+  mean: "Nhãn hiệu kinh doanh (n)"
+}, {
+  word: "essential",
+  mean: "Cần thiết, không thể thiếu (adj)"
+}, {
+  word: "prerequisite",
+  mean: "Điều kiện tiên quyết (n)"
+}, {
+  word: "surface",
+  mean: "Bề mặt (n)"
+}, {
+  word: "smooth",
+  mean: "Mịn, êm ả, mượt mà (adj)"
+}, {
+  word: "source",
+  mean: "Nguồn, nguồn gốc (n)"
+}, {
+  word: "resource",
+  mean: "Tài nguyên, nguồn tài nguyên (n)"
+}, {
+  word: "disagreement",
+  mean: "Bất đồng quan điểm, bất đồng ý kiến, sự bất đồng (n)"
+}, {
+  word: "argument",
+  mean: "Tranh cãi, sự tranh luận (n)"
+}, {
+  word: "disagree",
+  mean: "Không đồng ý (v)"
+}, {
+  word: "crime",
+  mean: "Tội phạm (n)"
+}, {
+  word: "tension",
+  mean: "Sự căng thẳng, sự hồi hộp (n)"
+}, {
+  word: "nervousness",
+  mean: "Sự lo lắng (n)"
+}, {
+  word: "stationery",
+  mean: "Văn phòng phẩm, đồ dùng học tập, đồ dùng văn phòng (n)"
+}, {
+  word: "carrier",
+  mean: "Người vận chuyển, người đưa thư, vận chuyển (n)"
+}, {
+  word: "catalog",
+  mean: "Danh mục (n)"
+}, {
+  word: "fulfill",
+  mean: "Thực hiện, tiến hành (những điều đã hứa sẽ làm) (v)"
+}, {
+  word: "integral",
+  mean: "Một phần không thể thiếu (adj)"
+}, {
+  word: "minimize",
+  mean: "Giảm thiểu, giảm tới mức tối thiểu (v)"
+}, {
+  word: "on hand",
+  mean: "Có sẵn để dùng (adj)"
+}, {
+  word: "sufficient",
+  mean: "Đủ, vừa đủ, đủ dùng (adj) = enough"
+}, {
+  word: "evidence",
+  mean: "Chứng cớ, bằng chứng (n)"
+}, {
+  word: "scientific",
+  mean: "Khoa học, thuộc về khoa học (adj)"
+}, {
+  word: "insufficient",
+  mean: "Không đủ (adj)"
+}, {
+  word: "compile",
+  mean: "Biên soạn, thu thập tài liệu (v)"
+}, {
+  word: "impose",
+  mean: "Áp đặt, bắt chịu (v)"
+}, {
+  word: "prompt",
+  mean: "Nhanh chóng, ngay lập tức (adj)"
+}, {
+  word: "prompt",
+  mean: "Gây ra, thúc đầy (v)"
+}, {
+  word: "rectify",
+  mean: "Khắc phục, sửa chữa (v)"
+}]
