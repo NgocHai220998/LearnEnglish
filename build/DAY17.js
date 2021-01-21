a7ea645db169a370a8dae2cf6011fd3b40fd7a22
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-01-22 04:06:27 +0700

    Add Day 17

---
+++ build/DAY17.js
@@ -0,0 +1,49 @@
+const day17 = [{
+  word: "regardless",
+  mean: "Bất chấp, mặc dù (adj)"
+}, {
+  word: "admit",
+  mean: "Cho vào, nhận vào (v)"
+}, {
+  word: "authorization",
+  mean: "Sự cho phép, sự cấp phép, sự phê chuẩn (n) - authorize(v) ủy quyền, cho phép"
+}, {
+  word: "escort",
+  mean: "Người dẫn đường, người hộ tống, người chỉ dẫn (n)"
+}, {
+  word: "identify",
+  mean: "Nhận dạng, định dạng, định danh (v)"
+}, {
+  word: "pertinent",
+  mean: "Có liên quan, có mối liên hệ (adj)"
+}, {
+  word: "procedure",
+  mean: "Thủ tục, quy trình (n)"
+}, {
+  word: "statement",
+  mean: "Bản kê khai, bản báo cáo tài chính (n)"
+}, {
+  word: "usual",
+  mean: "Thường lệ, thong thường, thường dùng (adj)"
+}, {
+  word: "factor",
+  mean: "Nhân tố, yếu tố tạo thành (n)"
+}, {
+  word: "interaction",
+  mean: "Sự tương tác, sự ảnh hưởng qua lại lẫn nhau (n) - interact (v) tương tác"
+}, {
+  word: "keep track of",
+  mean: "Theo dõi, giám sat (v)  = monitor (v, n)"
+}, {
+  word: "potential",
+  mean: "Tiềm năng, khả năng (ngầm ) (adj)"
+}, {
+  word: "sense",
+  mean: "Giác quan, khả năng cảm nhận(mùi, âm thanh, hình ảnh, vị ...) (n)"
+}, {
+  word: "voluntary",
+  mean: "Tình nguyện (adj) volunteer (n) tình nguyện viên"
+}, {
+  word: "",
+  mean: ""
+}]
